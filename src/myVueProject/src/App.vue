a691f900550148b3ca35c039ab61fcf4bee4a2cf
linan3 <329336545@qq.com>
2018-11-05 22:07:38 +0800

    2018-11-5 vue 学习

--- src/myVueProject/src/App.vue
+++ src/myVueProject/src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <h1 v-html='title'></h1>
+    <input v-model="newItem" v-on:keyup.enter="addNew">
+    <ul>
+      <li  v-for="item in items" :class="{finished:item.isFinished}" v-on:click="aaaaa(item)">
+        {{item.label}}
+      </li>
+    </ul>
     <HelloWorld/>
   </div>
 </template>
@@ -12,6 +19,30 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data:function(){
+    return {
+       title: '<span>2018-11-5：</span>TODOList',
+       items:[
+        
+       ],
+       liclass:'thisisliclass',
+       newItem:''
+    }
+  },
+  methods:{
+    aaaaa:function(a){
+      a.isFinished=!a.isFinished
+      console.log(a);
+    },
+    addNew:function(){
+      console.log(this.newItem);
+      this.items.push({
+        label:this.newItem,
+        isFinished:false
+      })
+      this.newItem = '';
+    }
   }
 }
 </script>
@@ -24,5 +55,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.finished{
+  text-decoration: underline;
 }
 </style>
